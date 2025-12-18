a6172a767529339cf70b1c453ce8cb41b339946d
GPPerez <gpperez@gmail.com>
2025-12-18 12:09:43 -0300

    [ADD]renee_payment_group_fix

---
+++ renee_payment_group_fix/__manifest__.py
@@ -0,0 +1,19 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Renee Payment Group Fix',
+    'version': '15.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Fix validation error in account_payment_group during installation',
+    'author': 'Calyx Servicios S.A',
+    'maintainer': 'Facundo Alaniz',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'account_payment_group',
+    ],
+    'data': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+}
